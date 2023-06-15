0350ba66aa2f8865d049c9bc69b94246774a865e
Ellaine-Addamms <ellaine.addamms@gmail.com>
2023-06-15 11:29:23 -0700

    new commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 /**
- * main - main block
- * Return : 0
+ * main - Entry point
+ *
+ * Description:  prints the size of various types 
+ *
+ * Return : Always 0 (Success)
  */
 int main(void)
 {
