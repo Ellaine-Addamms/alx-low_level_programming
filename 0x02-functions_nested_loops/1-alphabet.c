d8a01e2e791d9377e85515e5747a6bda3735dfb9
Ellaine-Addamms <ellaine.addamms@gmail.com>
2023-06-20 15:41:57 -0700

    new commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print out the alphabet
+ * main - uses built in function
+ * Return: 0 (success)
+ */
+
+void print_alphabet(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
+_putchar('\n');
+}
